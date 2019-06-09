1b0fd37ecb90bdb56ebb542471505a0bfb341827
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-09 15:52:25 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,8 +2,9 @@ def using_until
   levitation_force = 0
   loop do 
     puts "Wingardium Leviosa"
+    levitation_force += 1
   until levitation_force == 10
   break
- levitation_force += 1
+
 end
 
